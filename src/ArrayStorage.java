cc297f4a803d96c47959925cf4e6c64ca263ba54
Evgeny Rudoy <rudoy95@mail.ru>
2021-06-08 22:52:46 +0300

    Minor update of delete-method

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,12 +6,10 @@ public class ArrayStorage {
     private int size = 0;
 
     void clear() {
-        if (size > 0) {
-            for (int i = 0; i < size; i++) {
-                storage[i] = null;
-            }
-            size = 0;
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
         }
+        size = 0;
     }
 
     void save(Resume r) {
@@ -30,40 +28,27 @@ public class ArrayStorage {
 
     void delete(String uuid) {
         // удаляем запрашиваемый элемент
+        int indexOfNullResume = 0;
         for (int i = 0; i < size; i++) {
             if (storage[i].toString().equals(uuid)) {
                 storage[i] = null;
+                indexOfNullResume = i;     // порядковый номер, где образовался null
                 break;
             }
         }
         size--;
 
-        // определяем индекс, где образовался null между резюме
-        int indexOfNullResume = 0;
-        for (int i = 0; i < size + 1; i++) {
-            if (storage[i] == null) {
-                indexOfNullResume = i;
-                break;
-            }
-        }
-
         // ликвидируем null между резюме
-        for (int i = indexOfNullResume + 1; i < size; i++) {
-            storage[i - 1] = storage[i];
-            storage[i] = null;
-        }
+        System.arraycopy(storage, indexOfNullResume + 1, storage, indexOfNullResume, storage.length - indexOfNullResume - 1);
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        if (size > 0) {
-            Resume[] real_storage = new Resume[size];
-            System.arraycopy(storage, 0, real_storage, 0, size);
-            return real_storage;
-        }
-        return null;
+        Resume[] allResume = new Resume[size];
+        System.arraycopy(storage, 0, allResume, 0, size);
+        return allResume;
     }
 
     int size() {
