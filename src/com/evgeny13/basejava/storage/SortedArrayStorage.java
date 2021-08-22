568629afa7e1e6c8bc527f9a9fc4604045ada844
Evgeny Rudoy <rudoy95@mail.ru>
2021-08-22 23:45:47 +0300

    Added Comparator in SortedArrayStorage.java

--- src/com/evgeny13/basejava/storage/SortedArrayStorage.java
+++ src/com/evgeny13/basejava/storage/SortedArrayStorage.java
@@ -3,19 +3,28 @@ package com.evgeny13.basejava.storage;
 import com.evgeny13.basejava.model.Resume;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
-
-    @Override
-    protected Integer getSearchKey(String uuid) {
-        Resume searchKey = new Resume(uuid);
-        return Arrays.binarySearch(storage, 0, size, searchKey);
+/*
+    private static class ResumeComparator implements Comparator<Resume> {
+        @Override
+        public int compare(Resume o1, Resume o2) {
+            return o1.getUuid().compareTo(o2.getUuid());
+        }
     }
+*/
+
+    private static final Comparator<Resume> RESUME_COMPARATOR = new Comparator<Resume>() {
+        @Override
+        public int compare(Resume o1, Resume o2) {
+            return o1.getUuid().compareTo(o2.getUuid());
+        }
+    };
 
     @Override
     protected void fillDeletedElement(int index) {
         int numMoved = size - index - 1;
-
         if (numMoved > 0) {
             System.arraycopy(storage, index + 1, storage, index, numMoved);
         }
@@ -23,10 +32,15 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     protected void insertElement(Resume r, int index) {
-//        http://codereview.stackexchange.com/questions/36221/binary-search-for-inserting-in-array#answer-36239
+//      http://codereview.stackexchange.com/questions/36221/binary-search-for-inserting-in-array#answer-36239
         int insertIdx = -index - 1;
-
         System.arraycopy(storage, insertIdx, storage, insertIdx + 1, size - insertIdx);
         storage[insertIdx] = r;
+    }
+
+    @Override
+    protected Integer getSearchKey(String uuid) {
+        Resume searchKey = new Resume(uuid);
+        return Arrays.binarySearch(storage, 0, size, searchKey, RESUME_COMPARATOR);
     }
 }
