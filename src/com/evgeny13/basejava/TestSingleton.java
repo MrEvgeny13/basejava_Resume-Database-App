74217c2625ef65acad519fbb1a2ceb600dd42cad
Evgeny Rudoy <rudoy95@mail.ru>
2021-09-12 21:05:04 +0300

    Added SectionType.java - Enum with characteristics for Resume.java

---
+++ src/com/evgeny13/basejava/TestSingleton.java
@@ -0,0 +1,31 @@
+package com.evgeny13.basejava;
+
+import com.evgeny13.basejava.model.SectionType;
+
+public class TestSingleton {
+    private static TestSingleton instance;
+
+    public static TestSingleton getInstance() {
+        if (instance == null) {
+            instance = new TestSingleton();
+        }
+        return instance;
+    }
+
+    private TestSingleton() {
+    }
+
+    public static void main(String[] args) {
+        TestSingleton.getInstance().toString();
+        Singleton instance = Singleton.valueOf("INSTANCE");
+        System.out.println(instance.ordinal());
+
+        for (SectionType type : SectionType.values()) {
+            System.out.println(type.getTitle());
+        }
+    }
+
+    public enum Singleton {
+        INSTANCE
+    }
+}
