b97074259cf7d1e9cd592092d22853b27c75e057
Evgeny Rudoy <rudoy95@mail.ru>
2021-11-19 10:56:41 +0300

    Refactoring of the deadlock case

--- src/com/evgeny13/basejava/MainConcurrency.java
+++ src/com/evgeny13/basejava/MainConcurrency.java
@@ -58,29 +58,6 @@ public class MainConcurrency {
             }
         });
         System.out.println(mainConcurrency.counter);
-
-        final String lock1 = "lock1";
-        final String lock2 = "lock2";
-        deadLock(lock1, lock2);
-        deadLock(lock2, lock1);
-    }
-
-    private static void deadLock(Object lock1, Object lock2) {
-        new Thread(() -> {
-            System.out.println("Waiting for: " + lock1);
-            synchronized (lock1) {
-                System.out.println("Retention: " + lock1);
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("Waiting for: " + lock2);
-                synchronized (lock2) {
-                    System.out.println("Retention: " + lock2);
-                }
-            }
-        }).start();
     }
 
     private synchronized void inc() {
