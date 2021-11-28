63bf1b5b35fe863546c60ee6af156cfdbd4f2474
Evgeny Rudoy <rudoy95@mail.ru>
2021-11-28 05:55:34 +0300

    Refactoring (2)

--- src/com/evgeny13/basejava/storage/SqlStorage.java
+++ src/com/evgeny13/basejava/storage/SqlStorage.java
@@ -7,22 +7,12 @@ import com.evgeny13.basejava.util.SqlHelper;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.logging.Logger;
 
 public class SqlStorage implements Storage {
     private final SqlHelper sqlHelper;
     private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
-    private static final String SELECT_RESUME = "SELECT * FROM resume WHERE uuid=?";
-    private static final String DELETE_RESUMES = "DELETE FROM resume";
-    private static final String INSERT_RESUME = "INSERT INTO resume (uuid, full_name) VALUES (?,?)";
-    private static final String UPDATE_RESUME = "UPDATE resume SET full_name = ? WHERE uuid=?";
-    private static final String DELETE_RESUME = "DELETE FROM resume WHERE uuid =?";
-    private static final String SELECT_RESUMES = "SELECT * FROM resume ORDER BY full_name, uuid";
-    private static final String COUNT_RESUMES = "SELECT COUNT(*) from resume";
 
     public SqlStorage(String dbUrl, String dbUser, String dbPassword) {
         sqlHelper = new SqlHelper(() -> DriverManager.getConnection(dbUrl, dbUser, dbPassword));
@@ -30,7 +20,7 @@ public class SqlStorage implements Storage {
 
     @Override
     public void clear() {
-        sqlHelper.execute(DELETE_RESUMES);
+        sqlHelper.execute("DELETE FROM resume");
     }
 
     @Override
@@ -40,7 +30,7 @@ public class SqlStorage implements Storage {
         return sqlHelper.doTransaction(connection -> {
             Resume resume;
 
-            try (PreparedStatement ps = connection.prepareStatement(SELECT_RESUME)) {
+            try (PreparedStatement ps = connection.prepareStatement("SELECT * FROM resume WHERE uuid=?")) {
                 ps.setString(1, uuid);
                 ResultSet rs = ps.executeQuery();
 
@@ -57,14 +47,15 @@ public class SqlStorage implements Storage {
     @Override
     public void update(Resume resume) {
         LOG.info("Update " + resume);
+        String resumeUuid = resume.getUuid();
 
         sqlHelper.doTransaction(connection -> {
-            try (PreparedStatement ps = connection.prepareStatement(UPDATE_RESUME)) {
+            try (PreparedStatement ps = connection.prepareStatement("UPDATE resume SET full_name = ? WHERE uuid=?")) {
                 ps.setString(1, resume.getFullName());
-                ps.setString(2, resume.getUuid());
+                ps.setString(2, resumeUuid);
 
                 if (ps.executeUpdate() != 1) {
-                    throw new NotExistStorageException(resume.getUuid());
+                    throw new NotExistStorageException(resumeUuid);
                 }
             }
             return null;
@@ -76,7 +67,7 @@ public class SqlStorage implements Storage {
         LOG.info("Save " + resume);
 
         sqlHelper.doTransaction(connection -> {
-                    try (PreparedStatement ps = connection.prepareStatement(INSERT_RESUME)) {
+                    try (PreparedStatement ps = connection.prepareStatement("INSERT INTO resume (uuid, full_name) VALUES (?,?)")) {
                         ps.setString(1, resume.getUuid());
                         ps.setString(2, resume.getFullName());
                         ps.execute();
@@ -90,7 +81,7 @@ public class SqlStorage implements Storage {
     public void delete(String uuid) {
         LOG.info("Delete " + uuid);
 
-        sqlHelper.execute(DELETE_RESUME, ps -> {
+        sqlHelper.execute("DELETE FROM resume WHERE uuid =?", ps -> {
             ps.setString(1, uuid);
 
             if (ps.executeUpdate() == 0) {
@@ -105,22 +96,21 @@ public class SqlStorage implements Storage {
         return sqlHelper.doTransaction(conn -> {
             Map<String, Resume> resumes = new LinkedHashMap<>();
 
-            try (PreparedStatement ps = conn.prepareStatement(SELECT_RESUMES)) {
+            try (PreparedStatement ps = conn.prepareStatement("SELECT * FROM resume ORDER BY full_name, uuid")) {
                 ResultSet rs = ps.executeQuery();
 
                 while (rs.next()) {
-                    String uuid = rs.getString("uuid");
+                    String uuid = rs.getString("uuid").replace(" ", "");
                     resumes.put(uuid, new Resume(uuid, rs.getString("full_name")));
                 }
             }
-
             return new ArrayList<>(resumes.values());
         });
     }
 
     @Override
     public int size() {
-        return sqlHelper.execute(COUNT_RESUMES, st -> {
+        return sqlHelper.execute("SELECT COUNT(*) from resume", st -> {
             ResultSet rs = st.executeQuery();
             return rs.next() ? rs.getInt(1) : 0;
         });
